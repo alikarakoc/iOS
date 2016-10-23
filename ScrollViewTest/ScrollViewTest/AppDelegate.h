fbe0baead0a9e4554a83b4313d1d703163e68b9e
Ali Karakoç <bykrkc@gmail.com>
2016-10-23 21:33:01 +0300

    ScrollViewTest
    ScrollView Test projesi

---
+++ ScrollViewTest/ScrollViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewTest
+//
+//  Created by Ali Karakoç on 23.10.2016.
+//  Copyright © 2016 Ali Karakoç. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
